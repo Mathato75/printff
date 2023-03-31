be6830a3bf23dee284fccb05df6cddc36b051f60
Mathato75 <Tshego75@gmail.com>
2023-03-31 11:17:57 -0700

    wetry

--- utils.c
+++ utils.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
-/**
- * is_printable - Evaluates if a char is printable
- * @c: Char to be evaluated.
- *
- * Return: 1 if c is printable, 0 otherwise
+/*
+ * is_pintable - Evaluates ifdhfhfhf a chr is printable
+ * @c: Char to bjhgkhgkhk
+ *fdhfhfkh
+ * Retur: hkhkhhkhhkhgk1 if rintable, 0 otherwise
  */
 int is_printable(char c)
 {
@@ -14,17 +14,12 @@ int is_printable(char c)
 	return (0);
 }
 
-/**
- * append_hexa_code - Append ascci in hexadecimal code to buffer
- * @buffer: Array of chars.
- * @i: Index at which to start appending.
- * @ascii_code: ASSCI CODE.
- * Return: Always 3
+/**gjg coce ceic why the apcd 3
  */
 int append_hexa_code(char ascii_code, char buffer[], int i)
 {
 	char map_to[] = "0123456789ABCDEF";
-	/* The hexa format code is always 2 digits long */
+	
 	if (ascii_code < 0)
 		ascii_code *= -1;
 
@@ -38,11 +33,7 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
 }
 
 /**
- * is_digit - Verifies if a char is a digit
- * @c: Char to be evaluated
- *
- * Return: 1 if c is a digit, 0 otherwise
- */
+ * is_digit - Verifies ifgfgfjgj*/
 int is_digit(char c)
 {
 	if (c >= '0' && c <= '9')
@@ -51,12 +42,7 @@ int is_digit(char c)
 	return (0);
 }
 
-/**
- * convert_size_number - Casts a number to the specified size
- * @num: Number to be casted.
- * @size: Number indicating the type to be casted.
- *
- * Return: Casted value of num
+/**ffdhfhfhghgh
  */
 long int convert_size_number(long int num, int size)
 {
@@ -69,12 +55,7 @@ long int convert_size_number(long int num, int size)
 }
 
 /**
- * convert_size_unsgnd - Casts a number to the specified size
- * @num: Number to be casted
- * @size: Number indicating the type to be casted
- *
- * Return: Casted value of num
- */
+ dsjg''dsgdgdsgdgdgdg;dg;g;gfgfghhgi;hg;hgfgha;oiufa;oiufa;oiufa;oiufa;oiufa;oiufa;oiufa;oiufa;oiufa;oiufa;oiufa;oiufa;oiufa;oiufa;oiufa;oiufa;oiufa;oiufa;oiufa;oiufa;oiufa;oiufa;oiufa;oiufa;oiufa;oiufa;oiufa;oiufa;oiufa;oiufa;oiufa;oiufa */
 long int convert_size_unsgnd(unsigned long int num, int size)
 {
 	if (size == S_LONG)
